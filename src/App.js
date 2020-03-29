a6b84a63167ee49bba841877f4647b35746ed83d
kurodatkashi <autumn-ijxqe@ezweb.ne.jp>
2020-03-29 20:47:06 +0900

    feat: ハンバーガーメニューを追加。

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  let [bool, menuFlag] = useState('undefined');
+
+  const setBoolInMenuFlag = () => {
+    menuFlag(!bool);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrap">
+      <div className="bg">
+        <div className="line-wave"></div>
+      </div>
+      <div onClick={setBoolInMenuFlag} className="hamburger-menu">
+        <div>
+          <span className={`hamburger-menu__line${
+            !bool === true ? ' is-active'
+            : bool === 'undefined' ? ''
+            :' is-disabled'
+          }`}></span>
+          <span className={`hamburger-menu__line${
+            !bool === true ? ' is-active'
+            : bool === 'undefined' ? ''
+            :' is-disabled'
+          }`}></span>
+          <span className={`hamburger-menu__line${
+            !bool === true ? ' is-active'
+            : bool === 'undefined' ? ''
+            :' is-disabled'
+          }`}></span>
+        </div>
+      </div>
+      <div className={`menu${
+        !bool === true ? ' is-active'
+        : bool === 'undefined' ? ''
+        :' is-disabled'
+        }`}>
+        <div className="menu__inner">
+          <ul className="menu__list">
+            <li className="menu__item">
+              <img src="../img/home.svg" alt=""/>
+            </li>
+            <li className="menu__item">
+              <img src="../img/work.svg" alt=""/>
+            </li>
+            <li className="menu__item">
+              <img src="../img/about.svg" alt=""/>
+            </li>
+            <li className="menu__item">
+              <img src="../img/mail.svg" alt=""/>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <header>
+        <div className="logo">
+          <img src="../img/aki_is_logo_white.svg" alt=""/>
+        </div>
       </header>
+      <div className="fv"></div>
     </div>
   );
 }
